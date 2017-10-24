19c97337ff70b8679dec39720323a90330bcd1c3
Layla <laylacsa@msn.com>
2017-10-24 15:00:02 -0200

    Layout base da página Estudos

--- app/tabs/estudo/estudo.component.ts
+++ app/tabs/estudo/estudo.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from "@angular/core";
     templateUrl: "./estudo.component.html"
 })
 export class EstudoComponent implements OnInit {
+
+    area: number;
+    /*  studyGroups = 0,
+        lessonExchange = 1,
+        privateLessons = 2,
+    */
+
     constructor() {
         /* ***********************************************************
         * Use the constructor to inject services.
@@ -13,8 +20,30 @@ export class EstudoComponent implements OnInit {
     }
 
     ngOnInit(): void {
+        this.area = 4;
         /* ***********************************************************
         * Use the "ngOnInit" handler to initialize data for the view.
         *************************************************************/
+    }
+
+    checkIf(area: number) {
+        if (this.area == area) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    studyGroups() {
+        this.area = 0;
+    }
+
+    lessonExchange() {
+        this.area = 1;
+    }
+
+    privateLessons() {
+        this.area = 2;
     }
 }
