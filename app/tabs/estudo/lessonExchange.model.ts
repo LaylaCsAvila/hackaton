ba25e2666d26f9abbc69b66202aab5787d30e315
Layla <laylacsa@msn.com>
2017-10-25 02:48:49 -0200

    LessonExchange pronto

--- app/tabs/estudo/lessonExchange.model.ts
+++ app/tabs/estudo/lessonExchange.model.ts
@@ -1,9 +1,17 @@
 import { User } from "../../shared/user/user.model";
+import { BasePost } from "../../shared/models/base-post"
 
-export class lessonExchange {
-    owner: User;
-    postTitle: string;
+export class lessonExchange extends BasePost {
     subject: string;
-    text: string;
-    tags: string[];
+    classesOfInterest: string[];
+    major: string;
+
+    constructor(postTitle: string, observations: string, tags: string[],
+        user: User, maxPeople: number, subject: string, classes: string[],
+        major: string){
+        super(postTitle, observations, tags, user);
+        this.subject = subject;
+        this.classesOfInterest = classes;
+        this.major = major;
+    }
 }
